ae4deba749443c95ca31ad8bb572fd02a2bcb2f2
yangfangmin <fangmin.yang@ximalaya.com>
2024-07-14 14:00:57 +0800

    cp5

---
+++ kafka-in-action/chapter5/src/main/java/org/cola/kafkainaction/consumer/AlertTrendConsumer.java
@@ -0,0 +1,57 @@
+package org.cola.kafkainaction.consumer;
+
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+import org.cola.serde.AlertKeySerde;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.*;
+
+public class AlertTrendConsumer {
+
+    final static Logger log = LoggerFactory.getLogger(AlertTrendConsumer.class);
+    private volatile boolean keepConsuming = true;
+
+    public static void main(String[] args) {
+        Properties kaProperties = new Properties();
+        kaProperties.put("bootstrap.servers", "localhost:9092,localhost:9093");
+        kaProperties.put("enable.auto.commit", "true");
+        kaProperties.put("group.id", "kinaction_team0groupalerttrend");
+        kaProperties.put("key.deserializer", AlertKeySerde.class.getName());
+        kaProperties.put("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+
+        AlertTrendConsumer consumer = new AlertTrendConsumer();
+        consumer.consume(kaProperties);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(consumer::shutdown));
+    }
+
+    private void shutdown() {
+        keepConsuming = false;
+    }
+
+
+    private void consume(final Properties kaProperties) {
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(kaProperties)) {
+
+            consumer.subscribe(Collections.singletonList("kinaction_alerttrend"));
+
+            while (keepConsuming) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(250));
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("kinaction_info offset = {}, value = {}",
+                            record.offset(), record.value());
+                }
+            }
+        }
+
+    }
+
+}
